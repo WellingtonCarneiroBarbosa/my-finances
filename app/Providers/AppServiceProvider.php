346acb050b5ba6b72da2cc9760250f99105173c0
Wellington <carneirobarbosawellington@gmail.com>
2023-01-18 03:13:25 -0300

    add tools

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment('local')) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
@@ -23,6 +28,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Model::shouldBeStrict();
+
+        Request::macro('isApi', function (): bool {
+            return $this->is('api/*') && $this->expectsJson();
+        });
+
+        Str::macro('withoutNumbers', function (string $str) {
+            return preg_replace('/[0-9]+/', '', $str);
+        });
+
+        Str::macro('onlyNumbers', function (string $str) {
+            return preg_replace('/[^0-9]/', '', $str);
+        });
     }
 }
